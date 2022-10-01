652fa7fb61f332a3b28b8f69fad91bb716e008b2
Igor Potemkin <ipotemkin@rambler.ru>
2022-10-01 13:56:17 +0300

    feat: prepare the project structure

---
+++ src/slices/userSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { User } from '../models';
+import { RootState } from '../store';
+
+const initialState: User = {
+  id: -1,
+  username: '',
+  password: '',
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+  }
+});
+
+export const selectUser = (state: RootState) => state.user;
+
+export default userSlice.reducer;
