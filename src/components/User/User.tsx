222cbc8c1c041ea365c8530791424597cddb9cb0
Daria <daria.bnn@bk.ru>
2022-10-12 23:57:56 +0300

    feat: add toltip for User

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { User as FirebaseUser } from 'firebase/auth'
 
 import { ReactComponent as Arrow } from './arrow-down.svg'
@@ -10,11 +10,25 @@ type Props = {
 }
 
 export const User: FC<Props> = ({ user }) => {
+  const [isShowNav, setIsShowNav] = useState(false)
+
+  const handleShowNav = () => {
+    setIsShowNav((prev) => !prev)
+  }
+
   return (
-    <div className={styles.user}>
-      <div className={styles.avatar} />
-      <div className={styles.name}>{user.displayName || user.email }</div>
-      <Arrow />
+    <div className={styles.wrapper} onClick={handleShowNav}>
+      <div className={styles.user}>
+        <div className={styles.avatar} />
+        <div className={styles.name}>{user.displayName || user.email}</div>
+        <Arrow />
+      </div>
+      {isShowNav && (
+        <div className={styles.nav}>
+          <div className={styles.link}>Профиль</div>
+          <div className={styles.link}>Выйти</div>
+        </div>
+      )}
     </div>
   )
 }
