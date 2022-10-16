89d201edc34faebdc42f7aff534e4c433e8604d6
Igor Potemkin <ipotemkin@rambler.ru>
2022-10-16 23:40:22 +0300

    fefactor: Workout: add error handling

--- src/pages/WorkoutPage/Workout.tsx
+++ src/pages/WorkoutPage/Workout.tsx
@@ -16,27 +16,39 @@ import styles from './style.module.css'
 export const Workout: FC = () => {
   const { id, day } = useParams()
   const user = useAppSelector(selectUser)
-  const { userCourse, isLoading } = useUserCourse(user?.uid, Number(id))
+  const { data, isLoading, isError } = useUserCourse(user?.uid, Number(id) - 1)
 
-  if (isLoading) return <WarningPage text="Загрузка..." user={user} />
+  console.log('Workout: user -->', user)  // for DEBUG!
 
-  if (!user) return <WarningPage text="Вы не авторизованы!" />
+  if (isLoading || user.isLoading || (!data && !isError))
+    return <WarningPage text="Загрузка..." user={user} />
 
-  if (!userCourse)
+  if (!user.uid) return <WarningPage text="Вы не авторизованы!" />
+
+  if (!data) {
+    console.log('Вы не зарегистрированы на этот курс!')  // for DEBUG!
     return (
       <WarningPage text="Вы не зарегистрированы на этот курс!" user={user} />
     )
+  }
+  
+  const workoutIdx = Number(day) - 1
 
-  const workout = userCourse.workouts![Number(day) - 1]
+  if (!data.workouts || (workoutIdx < 0 || workoutIdx >= data.workouts?.length))
+    return (
+      <WarningPage text="Нет такой тренировки на этом курсе!" user={user} />
+    )
 
+  const workout = data.workouts[workoutIdx]
+  
   return (
     <div className={styles.container}>
       <Navigation children={<User user={user} />} />
       <main className={styles.main}>
-        <h1 className={styles.heading}>{userCourse.name}</h1>
+        <h1 className={styles.heading}>{data.name}</h1>
         <h2 className={styles.title}>{workout.name}</h2>
 
-        <VideoPlayer url={workout?.videoUrl || ''} />
+        <VideoPlayer url={workout.videoUrl || ''} />
 
         {workout.exercises!.length > 0 && (
           <div className={styles.exercises}>
