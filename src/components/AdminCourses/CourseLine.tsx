f9642a29674f253a878ff920441b13f54e1864fa
Igor Potemkin <ipotemkin@rambler.ru>
2022-10-22 13:35:32 +0300

    feat: add courses to a user

--- src/components/AdminCourses/CourseLine.tsx
+++ src/components/AdminCourses/CourseLine.tsx
@@ -1,5 +1,8 @@
 import { Button } from '@mui/material'
 import { FC } from 'react'
+import { useAddUserCourseMutation } from '../../api/users.api'
+import { useAppSelector } from '../../hooks/appHooks'
+import { selectUser } from '../../slices/userSlice'
 
 import { CourseData } from '../../types'
 
@@ -8,8 +11,19 @@ import styles from './style.module.css'
 type Props = { item: CourseData }
 
 export const CourseLine: FC<Props> = ({ item }) => {
+  const user = useAppSelector(selectUser)
+  const [addCourse] = useAddUserCourseMutation()
+
   const handleAddCourse = () => {
-    console.log('adding course:', item.id)
+    if (user && user.uid && item.id) {
+      addCourse({
+        uid: user.uid,
+        courseId: item.id
+      })
+      console.log('adding course:', item.id)
+    } else {
+      console.log('error adding course:', item.id)
+    }
   }
 
   const handleRemoveCourse = () => {
