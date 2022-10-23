21b73f9aa989c3b2f54f830e415676060c082e50
Igor Potemkin <ipotemkin@rambler.ru>
2022-10-23 20:22:13 +0300

    refactor: LoginForm: REST API

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,23 @@
+import { ERRORS } from "./constants"
+
 export const formatString = (text: string, args: string[]) => {
   let res = text
   for (const arg of args) {
     res = res.replace(/{}/, arg)
   }
   return res
+}
+
+type AuthErrorType = {
+  status: number
+  data: {
+    error: {
+      message: string
+    }
+  }
+}
+
+export const getErrorMessage = (error: AuthErrorType) => {
+  const testValue: string = ERRORS[error.data.error.message]
+  return testValue || 'Неверный логин или пароль'
 }
