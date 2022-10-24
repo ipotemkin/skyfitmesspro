dfa3c7054a3d58c308cd60c024d5baad4e809632
Igor Potemkin <ipotemkin@rambler.ru>
2022-10-24 19:39:28 +0300

    refactor: removed db folder, mod Admin

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,8 @@
-export const projectId = process.env.REACT_APP_PROJECT_ID
+// export const projectId = process.env.REACT_APP_PROJECT_ID
 export const apiKey = process.env.REACT_APP_API_KEY
-export const databaseURL = process.env.REACT_APP_DATABASE_URL
-export const authDomain = process.env.REACT_APP_AUTH_DOMAIN
-export const storageBucket = process.env.REACT_APP_STORAGE_BUCKET
-export const messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID
-export const appId = process.env.REACT_APP_APP_ID
-export const measurementId = process.env.REACT_APP_MEASUREMENT_ID
+// export const databaseURL = process.env.REACT_APP_DATABASE_URL
+// export const authDomain = process.env.REACT_APP_AUTH_DOMAIN
+// export const storageBucket = process.env.REACT_APP_STORAGE_BUCKET
+// export const messagingSenderId = process.env.REACT_APP_MESSAGING_SENDER_ID
+// export const appId = process.env.REACT_APP_APP_ID
+// export const measurementId = process.env.REACT_APP_MEASUREMENT_ID
