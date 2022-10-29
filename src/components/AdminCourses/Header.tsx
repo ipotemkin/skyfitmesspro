70b6dc58279f4739eb7a1392a6416673c043b930
Evgenia Fedorova <leleoartist@gmail.com>
2022-10-29 21:49:09 +0300

    feat: implement adaptive on Main page

--- src/components/AdminCourses/Header.tsx
+++ src/components/AdminCourses/Header.tsx
@@ -2,8 +2,7 @@ import styles from './style.module.css'
 
 export const Header = () => {
   return (
-    <div className={styles.header}
-    >
+    <div className={styles.header}>
       <div className={styles.col1}>№</div>
       <div className={styles.col2}>Курс</div>
       <div className={styles.col3}>Действие</div>
