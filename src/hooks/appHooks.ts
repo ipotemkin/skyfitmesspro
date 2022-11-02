b815947f850a3dcbf429781bb26b48df873f80d7
Igor Potemkin <ipotemkin@rambler.ru>
2022-11-02 11:18:35 +0300

    refactor: removed usage of react-cookie

--- src/hooks/appHooks.ts
+++ src/hooks/appHooks.ts
@@ -1,29 +1,9 @@
 // APPLICATION HOOKS
 
-import { useCookies } from 'react-cookie'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import { appCookiesNames } from '../constants'
 
 import type { AppDispatch, RootState } from '../store'
-import { AppCookies } from '../types'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
-
-export const useAppCookies = () => {
-  const [cookies, setCookie, removeCookie] = useCookies(appCookiesNames)
-
-  const setCookies = (cookiesArg: AppCookies) => {
-    for (let item in cookiesArg) {
-      setCookie(item, cookiesArg[item])
-    }
-  }
-
-  const removeCookies = () => {
-    // appCookiesNames.forEach(item => removeCookie(item))
-    Object.keys(cookies).forEach((item: string) => removeCookie(item))
-  }
-
-  return { cookies, setCookies, removeCookies }
-}
