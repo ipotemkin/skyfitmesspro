3aabfaa0a11c7db72a78286b3ff92395b33ef4c7
Evgenia Fedorova <leleoartist@gmail.com>
2022-11-02 20:21:56 +0300

    feat: add tests

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,6 +1,9 @@
 declare namespace Cypress {
   interface Chainable<Subject = any> {
     login(email: string, password: string): Chainable<void>
+    displayProgressButtonAndOpenModalWindow(): Chainable<void>
+    displayModalWindowAndSendData(data: number, max: number): Chainable<void>
+    displaySuccessModalWindow(): Chainable<void>
     displayCoursesAndOpenModalPage(
       course: number,
       workout: number
@@ -28,7 +31,7 @@ Cypress.Commands.add('login', (email: string, password: string) => {
 
 Cypress.Commands.add('displayCoursesAndOpenModalPage', (course, workout) => {
   cy.get('h4').contains('Мои курсы').should('exist')
-  cy.get('[data-cy="gallery-courses"]').should('exist').as('GalleryCourses')
+  cy.get('[data-cy="gallery-course"]').should('exist').as('GalleryCourses')
 
   cy.get('@GalleryCourses').children().eq(course).click()
 
@@ -43,4 +46,35 @@ Cypress.Commands.add('displayCoursesAndOpenModalPage', (course, workout) => {
     .click()
 
   cy.location('pathname').should('include', `/workouts/${workout}`)
+})
+
+Cypress.Commands.add('displayProgressButtonAndOpenModalWindow', () => {
+  cy.get('button')
+    .should('have.text', 'Заполнить свой прогресс')
+    .as('ProgressButton')
+  cy.get('@ProgressButton').click()
+  cy.get('div[data-cy="progress-modal"]').should('exist')
+})
+
+Cypress.Commands.add('displayModalWindowAndSendData', (data, max) => {
+  const value = data > 0 ? max.toString() : ''
+
+  cy.get('div[data-cy="progress-modal"]').should('exist').as('ProgressModal')
+  cy.get('@ProgressModal').within(() => {
+    cy.get('h2').contains('Мой прогресс').should('exist')
+    cy.root().find('input').as('Inputs')
+    cy.get('@Inputs')
+      .first()
+      .clear()
+      .type(data.toString())
+      .should('have.value', value)
+      .blur()
+
+    cy.get('button').contains('Отправить').click()
+  })
+
+  Cypress.Commands.add('displaySuccessModalWindow', () => {
+    cy.get('div[data-cy="success-modal"]').should('exist').as('SuccessModal')
+    cy.get('@SuccessModal').contains('Ваш прогресс засчитан!')
+  })
 })
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 
 import { Button } from '../../components/Button/Button'
+import { Footer } from '../../components/Footer/Footer'
 import { Gallery } from '../../components/Gallery/Gallery'
 import { Logo } from '../../components/Logo/Logo'
 import { LOGO_COLOR_LIGHT } from '../../constants'
@@ -15,34 +16,39 @@ export const Main = () => {
   const isLoggedIn = user && user.localId
 
   return (
-    <div className={styles.wrapper}>
-      <header className={styles.header}>
-        <nav className={styles.nav}>
-          <Logo color={LOGO_COLOR_LIGHT} />
-          <Link to={isLoggedIn ? ROUTES.profile : ROUTES.login}>
-            <Button type="tertiary" size="s">
-              Войти
-            </Button>
-          </Link>
-        </nav>
-        <h2 className={styles.subtitle}>Онлайн-тренировки для занятий дома</h2>
-        <div className={styles.titleWrapper}>
-          <h1 className={styles.title} id="top">
-            Начните заниматься спортом и&nbsp;улучшите качество жизни
-          </h1>
-          <div className={styles.sticker}>
-            Измени своё тело&nbsp;за&nbsp;полгода
+    <>
+      <div className={styles.wrapper}>
+        <header className={styles.header}>
+          <nav className={styles.nav}>
+            <Logo color={LOGO_COLOR_LIGHT} />
+            <Link to={isLoggedIn ? ROUTES.profile : ROUTES.login}>
+              <Button type="tertiary" size="s">
+                Войти
+              </Button>
+            </Link>
+          </nav>
+          <h2 className={styles.subtitle}>
+            Онлайн-тренировки для занятий дома
+          </h2>
+          <div className={styles.titleWrapper}>
+            <h1 className={styles.title} id="top">
+              Начните заниматься спортом и&nbsp;улучшите качество жизни
+            </h1>
+            <div className={styles.sticker}>
+              Измени своё тело&nbsp;за&nbsp;полгода
+            </div>
           </div>
-        </div>
-      </header>
-      <main className={styles.main}>
-        <Gallery />
-        <a href="#top">
-          <Button type="secondary" buttonStatus="normal" size="m">
-            Наверх ↑
-          </Button>
-        </a>
-      </main>
-    </div>
+        </header>
+        <main className={styles.main}>
+          <Gallery />
+          <a href="#top">
+            <Button type="secondary" buttonStatus="normal" size="m">
+              Наверх ↑
+            </Button>
+          </a>
+        </main>
+      </div>
+      <Footer />
+    </>
   )
 }
